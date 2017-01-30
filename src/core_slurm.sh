fd7b8c347e57c7adc5eeecb824c1c1883e4c5e0a
Daway Chou-Ren <dchouren@princeton.edu>
2017-01-30 00:01:17 -0500

    removed tabs

--- src/core_slurm.sh
+++ src/core_slurm.sh
@@ -18,29 +18,28 @@ function notify_email() {
 # $4 = job name
 # $5 = slurm output
 function gpu_slurm_header {
-  echo "#!/bin/sh
-  # Request runtime
-  #SBATCH --time=$1
-  # Request a number of CPUs per task:
-  #SBATCH --cpus-per-task=1
-  #SBATCH --ntasks=1
-  # Request a number of nodes:
-  #SBATCH --nodes=1
-  # Request an amount of memory per node:
-  #SBATCH --mem=$2
-  # Specify a job name:
-  #SBATCH -J $4
-  #SBATCH -o $4
-  # Request GPU stuff
-  #SBATCH -N 1 
-  #SBATCH --ntasks-per-node=1
-  #SBATCH --ntasks-per-socket=1 
-  #SBATCH --gres=gpu:1
-  # Set working directory:
-  #SBATCH --workdir=$SLURM_OUT
-  $5
-  srun /usr/bin/time -f '%E elapsed, %U user, %S system, %M memory, %x status' /bin/bash -c \"set -e $3\"" 
-  echo "in gpu slurm header"
+echo "#!/bin/sh
+# Request runtime
+#SBATCH --time=$1
+# Request a number of CPUs per task:
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+# Request a number of nodes:
+#SBATCH --nodes=1
+# Request an amount of memory per node:
+#SBATCH --mem=$2
+# Specify a job name:
+#SBATCH -J $4
+#SBATCH -o $4
+# Request GPU stuff
+#SBATCH -N 1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-socket=1 
+#SBATCH --gres=gpu:1
+# Set working directory:
+#SBATCH --workdir=$SLURM_OUT
+$5
+srun /usr/bin/time -f '%E elapsed, %U user, %S system, %M memory, %x status' /bin/bash -c \"set -e $3\"" 
 }
 
 
@@ -52,23 +51,23 @@ function gpu_slurm_header {
 # $4 = job name
 # $5 = slurm output
 function slurm_header() {
-  echo "#!/bin/sh
-  # Request runtime
-  #SBATCH --time=$1
-  # Request a number of CPUs per task:
-  #SBATCH --cpus-per-task=1
-  #SBATCH --ntasks=1
-  # Request a number of nodes:
-  #SBATCH --nodes=1
-  # Request an amount of memory per node:
-  #SBATCH --mem=$2
-  # Specify a job name:
-  #SBATCH -J $4
-  #SBATCH -o $4
-  # Set working directory:
-  #SBATCH --workdir=$SLURM_OUT
-  $5
-  srun /usr/bin/time -f '%E elapsed, %U user, %S system, %M memory, %x status' /bin/bash -c \"set -e $3\""
+echo "#!/bin/sh
+# Request runtime
+#SBATCH --time=$1
+# Request a number of CPUs per task:
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+# Request a number of nodes:
+#SBATCH --nodes=1
+# Request an amount of memory per node:
+#SBATCH --mem=$2
+# Specify a job name:
+#SBATCH -J $4
+#SBATCH -o $4
+# Set working directory:
+#SBATCH --workdir=$SLURM_OUT
+$5
+srun /usr/bin/time -f '%E elapsed, %U user, %S system, %M memory, %x status' /bin/bash -c \"set -e $3\""
 }
 
 
